d9268a521d588f2658a548a6c3ab10ba2327e10c
Thaliadobson <102420922+Thaliadobson@users.noreply.github.com>
2022-05-03 12:52:06 -0500

    Add files via upload

---
+++ Dobson.Thalia-PythonCode-ITT103-S2022 - Copy.py
@@ -0,0 +1,68 @@
+Author: Thalia Dobson
+Date Created:May 2,2022
+Course:ITT103
+Purpose: This python programme is used to calculate the commission. It is also used as a learning experience for the coder.
+
+ID_num=int(input("Enter the salesperson number"))
+sales_amount=float(input("Enter the sales amount"))
+class=int(input("Enter the class"))
+comm_rate=float
+commission=float
+
+
+def calcCommission():
+    commission=float(sales_amount * comm_rate)
+
+
+if cla_ss := 1:
+    sales_amount <= 1000 and sales_amount > 1000 < 2000 & sales_amount <= 2000
+    
+if cla_ss := 2:
+    sales_amount < 1000 and sales_amount >= 1000
+
+if cla_ss := 3:
+    comm_rate = 0.045
+        
+else:
+    if cla_ss := 4:
+        
+         print('Class not found')
+      
+           
+if sales_amount <=1000:                                                             
+        Commission = sales_amount * 0.06
+        
+elif    sales >1000 <2000:
+                Commission = sales_amount * 0.07
+                
+elif        sales <= 2000:
+                    Commission = sales_amount * 0.1
+                    
+elif            sales <1000:
+                        Commission = sales_amount * 0.04
+                        
+elif                 sales >=1000:
+                            Commission = sales_amount * 0.06 
+    else:
+                            sales > 0: 
+                                Commission = sales_amount * 0.045
+
+
+                                   
+print("for sales amount", sales_amount, "for commission", comm_rate, "for  sales person", ID_num, "with commission", commission)
+                                   
+print("Enter the salesperson number ", ID_num)
+print("Enter the sales amount ", sales_amount)
+print("Enter the class ", cla_ss)
+    
+    
+    
+
+    
+
+
+
+
+
+    
+                      
